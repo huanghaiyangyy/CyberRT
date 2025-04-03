f588ab5b8f8b9da402f802f85467ccc253cbe9cf
haiyang.huang <haiyang.huang@faw-vw.com>
2025-04-03 12:01:34 +0800

    feat: Reverse Engineering for NaviInfo proto file

--- cyber/examples/listener.cc
+++ cyber/examples/listener.cc
@@ -14,25 +14,174 @@
  * limitations under the License.
  *****************************************************************************/
 
-#include "cyber/examples/proto/examples.pb.h"
+#include <cyber/cyber.h>
+#include <google/protobuf/dynamic_message.h>
+#include <google/protobuf/message.h>
 
-#include "cyber/cyber.h"
+using apollo::cyber::Node;
+using apollo::cyber::Reader;
+using apollo::cyber::message::RawMessage;
 
-void MessageCallback(
-    const std::shared_ptr<apollo::cyber::examples::proto::Chatter>& msg) {
-  AINFO << "Received message seq-> " << msg->seq();
-  AINFO << "msgcontent->" << msg->content();
+bool generate_proto = true;
+void GenerateProto(const google::protobuf::Descriptor* descriptor,
+                   std::string& output) {
+  // 生成消息头
+  output += "message " + descriptor->name() + " {\n";
+
+  // 遍历所有字段
+  for (int i = 0; i < descriptor->field_count(); ++i) {
+    const google::protobuf::FieldDescriptor* field = descriptor->field(i);
+
+    // 生成字段定义
+    std::string field_rule;
+    if (field->is_repeated())
+      field_rule = "repeated ";
+    else if (field->is_optional())
+      field_rule = "optional ";
+
+    std::string type_name;
+    if (field->type() == google::protobuf::FieldDescriptor::TYPE_MESSAGE) {
+      type_name = field->message_type()->full_name();  // 嵌套消息的全名
+    } else {
+      type_name = field->type_name();  // 基础类型名（如 "int32"）
+    }
+
+    output += "  " + field_rule + type_name + " " + field->name() + " = " +
+              std::to_string(field->number()) + ";\n";
+  }
+
+  // 递归处理嵌套消息
+  for (int i = 0; i < descriptor->nested_type_count(); ++i) {
+    GenerateProto(descriptor->nested_type(i), output);
+  }
+
+  output += "}\n\n";
+}
+
+void GenerateProtoFile(std::string type_name) {
+  if (type_name.empty()) {
+    AERROR << "Failed to get message type name";
+    return;
+  }
+  // 通过Protobuf工厂获取消息描述符
+  auto* descriptor = apollo::cyber::message::ProtobufFactory::Instance()
+                         ->GenerateMessageByType(type_name)
+                         ->GetDescriptor();
+
+  // 语法版本
+  std::string output = "syntax = \"proto2\";\n\n";
+
+  // 包名
+  if (!descriptor->file()->package().empty()) {
+    output += "package " + descriptor->file()->package() + ";\n\n";
+  }
+
+  // 生成主消息
+  GenerateProto(descriptor, output);
+  // 将生成的内容写入文件
+  std::string file_name = descriptor->file()->name();
+  // 只保留文件名部分
+  file_name = file_name.substr(file_name.find_last_of('/') + 1);
+  std::ofstream proto_file(file_name);
+  if (proto_file.is_open()) {
+    proto_file << output;
+    proto_file.close();
+    AINFO << "Generated proto file: " << file_name;
+  } else {
+    AERROR << "Failed to open file: " << file_name;
+  }
+}
+void MessageCallback(const std::shared_ptr<RawMessage>& raw_msg) {
+  // 获取消息类型名称
+  const std::string type_name =
+      "apollo.common.StatusPb";  //"davinci_adas.apa.odometry.Odometry";
+  if (type_name.empty()) {
+    AERROR << "Failed to get message type name";
+    return;
+  }
+  if (generate_proto) {
+    GenerateProtoFile(type_name);
+    generate_proto = false;
+  }
+  return;
+  // 通过Protobuf工厂获取消息描述符
+  auto* descriptor = apollo::cyber::message::ProtobufFactory::Instance()
+                         ->GenerateMessageByType(type_name)
+                         ->GetDescriptor();
+  if (!descriptor) {
+    AERROR << "Failed to get descriptor for type: " << type_name;
+    return;
+  }
+
+  // 创建动态消息和工厂
+  google::protobuf::DynamicMessageFactory factory;
+  const google::protobuf::Message* prototype = factory.GetPrototype(descriptor);
+  if (!prototype) {
+    AERROR << "Failed to create prototype message";
+    return;
+  }
+
+  // 创建可修改的消息实例
+  std::unique_ptr<google::protobuf::Message> message(prototype->New());
+  if (!message->ParseFromString(raw_msg->message)) {
+    AERROR << "Failed to parse message data";
+    return;
+  }
+
+  // 使用反射遍历字段
+  const google::protobuf::Reflection* reflection = message->GetReflection();
+  std::vector<const google::protobuf::FieldDescriptor*> fields;
+  reflection->ListFields(*message, &fields);
+
+  AINFO << "Received message of type: " << descriptor->full_name();
+  for (const auto& field : fields) {
+    std::string value_str;
+
+    // 根据字段类型处理不同数据
+    if (field->is_repeated()) {
+      // 处理数组类型（示例仅打印首个元素）
+      const int count = reflection->FieldSize(*message, field);
+      if (count > 0) {
+        value_str = "[...] (repeated, size=" + std::to_string(count) + ")";
+      }
+    } else {
+      // 处理不同类型标量字段
+      switch (field->cpp_type()) {
+        case google::protobuf::FieldDescriptor::CPPTYPE_INT32:
+          value_str = std::to_string(reflection->GetInt32(*message, field));
+          break;
+        case google::protobuf::FieldDescriptor::CPPTYPE_STRING:
+          value_str = reflection->GetString(*message, field);
+          break;
+        case google::protobuf::FieldDescriptor::CPPTYPE_UINT32:
+          value_str = std::to_string(reflection->GetUInt32(*message, field));
+          break;
+        case google::protobuf::FieldDescriptor::CPPTYPE_FLOAT:
+          value_str = std::to_string(reflection->GetFloat(*message, field));
+          break;
+        case google::protobuf::FieldDescriptor::CPPTYPE_MESSAGE:
+          value_str = "{...} (sub-message)";
+          break;
+        // 添加其他类型的处理...
+        default: {
+          AERROR << "Unsupported field type: " << field->cpp_type();
+          value_str = "[Unhandled Type]";
+          break;
+        }
+      }
+    }
+
+    AINFO << "  " << field->name() << ": " << value_str
+          << " (type: " << field->cpp_type_name() << ")" << field->cpp_type();
+  }
 }
 
 int main(int argc, char* argv[]) {
-  // init cyber framework
   apollo::cyber::Init(argv[0]);
-  // create listener node
-  auto listener_node = apollo::cyber::CreateNode("listener");
-  // create listener
-  auto listener =
-      listener_node->CreateReader<apollo::cyber::examples::proto::Chatter>(
-          "channel/chatter", MessageCallback);
+  auto node = apollo::cyber::CreateNode("raw_reader");
+  auto reader =
+      node->CreateReader<RawMessage>("/davinci/apa/odometry", MessageCallback);
+
   apollo::cyber::WaitForShutdown();
   return 0;
 }
