3b3a10439287382818ebf90f33a8b1c5b747629b
haiyang.huang <haiyang.huang@faw-vw.com>
2025-05-08 11:07:32 +0800

    chore: Cyber record process.

--- scripts/record_process.py
+++ scripts/record_process.py
@@ -2,10 +2,10 @@ from cyber_record.record import Record
 import time
             
 def read_write_message():
-    read_file_name = "/home/huang/Cyber_Records/20250307160350.record.00000"
+    read_file_name = "/home/huang/Documents/CyberRT-7.0.0/Cyber_Records/20250424153431.record.00000"
     r_record = Record(read_file_name)
 
-    write_file_name = "/home/huang/Cyber_Records/20250307160350_out.record.00000"
+    write_file_name = "/home/huang/Documents/CyberRT-7.0.0/Cyber_Records/20250424153431_out.record.00000"
     with Record(write_file_name, mode='w') as w_record:
         for topic, message, t in r_record.read_messages_fallback():
             print("{}, {}, {}".format(topic, type(message), t))
